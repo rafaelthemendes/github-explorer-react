4c144f65e6e30198edee5b35b7fabebe8919594c
Rafael Mendes <rafaelthemendes@gmail.com>
2020-05-21 17:52:03 -0300

    :sparkles: repository details and issues list

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,13 +1,103 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { Link, useRouteMatch } from 'react-router-dom';
+import Logo from '../../assets/logo.svg';
+import { Header, Issues, RepositoryInfo } from './styles';
+import api from '../../services/api';
 
 interface RepositoryParams {
-  repository: string;
+  repositoryName: string;
 }
 
-const Repository: React.FC = () => {
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  user: {
+    login: string;
+  };
+  html_url: string;
+}
+
+const RepositoryDetails: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository: {params.repository}</h1>;
+  const [repository, setRepository] = useState<Repository | undefined>();
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const [repositoryResponse, issuesResponse] = await Promise.all([
+        api.get(`repos/${params.repositoryName}`),
+        api.get(`repos/${params.repositoryName}/issues`),
+      ]);
+      console.log(issuesResponse.data);
+      setRepository(repositoryResponse.data);
+      setIssues(issuesResponse.data);
+    };
+    loadData();
+  }, [params.repositoryName]);
+
+  return (
+    <>
+      <Header>
+        <img src={Logo} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft />
+          Voltar
+        </Link>
+      </Header>
+      {repository && (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Issues abertas</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      )}
+      <Issues>
+        {issues.map((issue) => (
+          <a key={issue.id} href={issue.html_url} target="_blank">
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Issues>
+    </>
+  );
 };
 
-export default Repository;
+export default RepositoryDetails;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,7 +6,7 @@ import Repository from '../pages/Repository';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/repository/:repository+" component={Repository} />
+    <Route path="/repository/:repositoryName+" component={Repository} />
   </Switch>
 );
 
